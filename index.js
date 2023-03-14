0561e4ec99dbd9efe2c3e53b314536cc34b5c82c
yazeedahmed <dee3ayahmed25@gmail.com>
2023-03-13 17:03:03 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 =31;
+let num2 =2;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()*10) +1;
+
+let num3 =104;
+let num4 =5;
+let mod = num3%num4;
+
+let max =Math.max(5,7,9,11,20);
